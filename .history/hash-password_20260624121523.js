c960c7c488e2a1204169f5e2331a1266782bc670
Paavo12 <110807050+Paavo12@users.noreply.github.com>
2026-06-24 12:23:22 +0200

    Fix login page placeholders, update admin views, and general improvements

---
+++ .history/hash-password_20260624121523.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+
+const password = '%De12Ameida987Teopilina!@#';
+const saltRounds = 10;
+
+bcrypt.hash(password, saltRounds, (err, hash) => {
+  if (err) throw err;
+  console.log('Hashed password:', hash);
+});
+teopolina12dealmeida@localink.com
+
+$2b$10$YmK/uTgWIT2Xz3JEKlhjLu6TnFxHOcZGlJJ72vSc2vPfNIsMhEELe
